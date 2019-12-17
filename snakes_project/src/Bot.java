c113c7687f23c9f9a356c42b64aaf4f46183fc24
Mahmood-Darwish <58790852+Mahmood-Darwish@users.noreply.github.com>
2019-12-17 07:06:58 +0200

    Add files via upload

---
+++ snakes_project/src/Bot.java
@@ -0,0 +1,13 @@
+package snakes;
+
+/*
+Interface from moodle and enum Direction from moodle
+ */
+
+enum Direction {
+    NORTH, SOUTH, WEST, EAST;
+}
+
+public interface Bot {
+    public abstract Direction chooseDirection(final Snake mySnake, final Snake otherSnake, final int[][] maze);
+}
---
+++ snakes_project/src/Coordinate.java
@@ -0,0 +1,20 @@
+package snakes;
+
+/*
+This class is the same from the lab but I have made the compareTo function work randomly so that every game will be different.
+ */
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Coordinate implements Comparable<Coordinate>{
+    int row;
+    int column;
+    public Coordinate(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    @Override
+    public int compareTo(Coordinate other) {
+        return ThreadLocalRandom.current().nextInt(-1, 2);
+    }
+}
