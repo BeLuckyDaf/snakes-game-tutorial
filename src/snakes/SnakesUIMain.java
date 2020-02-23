9790a239e1065f9bf3f43dbd1b9b0363ccd1d964
Vladislav Smirnov <beluckydaf@gmail.com>
2020-02-23 18:57:18 +0300

    Add arguments error message.

--- src/snakes/SnakesUIMain.java
+++ src/snakes/SnakesUIMain.java
@@ -3,6 +3,10 @@ package snakes;
 public class SnakesUIMain {
     /* UI Entry point */
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("You must provide two classes implementing the Bot interface.");
+            System.exit(1);
+        }
 
         BotLoader loader = new BotLoader();
         Bot firstBot = loader.getBotClass(args[0]);
@@ -10,8 +14,6 @@ public class SnakesUIMain {
 
         SnakeGame game = new SnakeGame(
                 new Coordinate(14, 14), // mazeSize
-
-
                 new Coordinate(4, 6), // head0
                 Direction.DOWN,         // tailDirection2
                 new Coordinate(7, 7), // head1
